363207f5d29a9c2291881be73585344889efd260
poly <206082275+chronopoly@users.noreply.github.com>
2025-04-10 16:20:24 +0300

    Allow Card to fetch the image from API

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,10 +1,19 @@
+import { useEffect, useState } from 'react'
 import '../styles/Card.css'
 
-function Card() {
+function Card( { pokemonName }) {
+    const [pokemonImg, setPokemonImg] = useState(null);
+
+    useEffect(() => {
+        fetch("https://pokeapi.co/api/v2/pokemon/" + pokemonName)
+        .then(response => response.json())
+        .then(response => setPokemonImg(response.sprites.other.dream_world.front_default));
+    })
+
     return (
         <div className="pokemon-card">
-            <img className="pokemon-image" src="" alt="Image of a pokemon" />
-            <p className="pokemon-name">Name of a pokemon</p>
+            <img className="pokemon-image" src={pokemonImg} alt="Image of a pokemon" />
+            <p className="pokemon-name">{pokemonName}</p>
         </div>
     )
 }
