42706db9ff6cb9fab015a11afa1da4a30d13a74a
poly <206082275+chronopoly@users.noreply.github.com>
2025-04-10 15:21:03 +0300

    Add html carcass for Card

---
+++ src/components/Card.jsx
@@ -0,0 +1,12 @@
+import '../styles/Card.css'
+
+function Card() {
+    return (
+        <div className="pokemon-card">
+            <img className="pokemon-image" src="" alt="Image of a pokemon" />
+            <p className="pokemon-name">Name of a pokemon</p>
+        </div>
+    )
+}
+
+export default Card
