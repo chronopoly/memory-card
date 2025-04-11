29625447ce587fbf206b2108b52b9615755f6a46
poly <206082275+chronopoly@users.noreply.github.com>
2025-04-11 14:11:59 +0300

    Create a function to shuffle arrays

---
+++ src/utils/shuffleArray.js
@@ -0,0 +1,18 @@
+export default function shuffleArray(array) {
+    let currentIndex = array.length;
+
+    // while there are still unshuffled element
+    while (currentIndex != 0) {
+        // create a random index
+        let randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+
+        // swap element at random index with element at current index
+        let temp = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temp;
+    }
+    console.log(array); 
+
+    return array;
+}
