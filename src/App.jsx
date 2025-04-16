30b3051d9b6a1aa4dd5e4ac7f68f8556f05fda6a
poly <206082275+chronopoly@users.noreply.github.com>
2025-04-16 14:40:01 +0300

    Add functionality to handle the score of the game

--- src/App.jsx
+++ src/App.jsx
@@ -4,29 +4,56 @@ import Card from './components/Card'
 import shuffleArray from './utils/shuffleArray';
 
 function App() {
+  const [topScore, setTopScore] = useState(0);
+  const [currentScore, setCurrentScore] = useState(0);
   const [pokemonList, setPokemonList] = useState([
-    "Bulbasaur",
-    "Charmander",
-    "Squirtle", 
-    "Caterpie", 
-    "Pidgey",
-    "Rattata",
-    "Pikachu",
-    "Vulpix",
-    "Gloom",
-    "Zubat",
-    "Meowth",
-    "Psyduck",
+    {name: "Bulbasaur", clicked: false},
+    {name: "Charmander", clicked: false},
+    {name: "Squirtle", clicked: false},
+    {name: "Caterpie", clicked: false},
+    {name: "Pidgey", clicked: false},
+    {name: "Rattata", clicked: false},
+    {name: "Pikachu", clicked: false},
+    {name: "Vulpix", clicked: false},
+    {name: "Gloom", clicked: false},
+    {name: "Zubat", clicked: false},
+    {name: "Meowth", clicked: false},
+    {name: "Psyduck", clicked: false},
   ]);
 
-  function handleClick() {
+  function shufflePokemon() {
     const newList = pokemonList;
     setPokemonList([...shuffleArray(newList)]);
   }
 
+  function handleScore(pokemon) {
+    if (pokemon.clicked) {
+      // if card has been clicked before set score to zero and mark all card as unclicked
+      setCurrentScore(0);
+      const newList = [...pokemonList];
+      newList.forEach(pokemon => {pokemon.clicked = false});
+      setPokemonList(newList);
+    } else {
+      // increment the score, update top score if needed, and mark card as clicked
+      setCurrentScore(currentScore + 1);
+      currentScore + 1 > topScore ? setTopScore(currentScore + 1) : null;
+      const newList = [...pokemonList];
+      newList.forEach(element => {
+        if (element.name === pokemon.name) element.clicked = true;
+      });
+      setPokemonList(newList);
+    }
+    shufflePokemon();
+  }
+
   return (
     <>
-      {pokemonList.map(pokemon => <Card pokemonName={pokemon} key={pokemon} onClick={handleClick} />)}
+      {pokemonList.map(pokemon => {
+        return <Card 
+          pokemon={pokemon}
+          key={pokemon.name}
+          handleScore={handleScore}/>
+        })}
     </>
   )
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,19 +1,25 @@
 import { useEffect, useState } from 'react'
 import '../styles/Card.css'
 
-function Card({ pokemonName, onClick }) {
+function Card({ pokemon, handleScore }) {
     const [pokemonImg, setPokemonImg] = useState(null);
 
+    function handleClick() {
+        // copy the pokemon object so we can pass it as an argument to handleScore in App
+        const pokemonCopy = {...pokemon};
+        handleScore(pokemonCopy);
+    }
+
     useEffect(() => {
-        fetch("https://pokeapi.co/api/v2/pokemon/" + pokemonName)
+        fetch("https://pokeapi.co/api/v2/pokemon/" + pokemon.name)
         .then(response => response.json())
         .then(response => setPokemonImg(response.sprites.other.dream_world.front_default));
-    }, [pokemonName])
+    }, [pokemon.name])
 
     return (
-        <div className="pokemon-card" onClick={onClick}>
-            <img className="pokemon-image" src={pokemonImg} alt={"Picture of a " + pokemonName} />
-            <p className="pokemon-name">{pokemonName}</p>
+        <div className="pokemon-card" onClick={handleClick}>
+            <img className="pokemon-image" src={pokemonImg} alt={"Picture of a " + pokemon.name} />
+            <p className="pokemon-name">{pokemon.name}</p>
         </div>
     )
 }
